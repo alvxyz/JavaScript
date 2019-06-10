e0c0109bfba78c045f7e0e706feb5367759a3c87
Alvian Teddy Cahya Putra <alvianteddy@gmail.com>
2019-06-10 09:37:54 +0700

    Kelas Terbuka

---
+++ Pengulangan/script.js
@@ -0,0 +1,14 @@
+var nilaiAwal = 1;
+
+while (nilaiAwal <= 10) {
+    console.log("Hello World " + nilaiAwal + "x");
+    nilaiAwal++;
+}
+
+
+// var ulang = true;
+
+// while (ulang) {
+//     console.log ("Hello Guys");
+//     ulang = confirm("Ulang lagi ga?");
+// }
---
+++ Popup Box/popup.js
@@ -0,0 +1,26 @@
+// alert('Halo Dunia Pemrograman :)');
+// alert('Halo nama saya Alvian');
+
+// var nama = prompt('Masukkan nama: ');
+// alert('Hallo ' + nama);
+
+// var tes = confirm('Kamu yakin?');
+
+// if (tes == true) {
+//     alert('User menekan tombol OK!');
+// } else {
+//     alert('User menekan tombol Batal');
+// }
+
+alert('Selamat Datang');
+
+var lagi = true;
+
+while(lagi) {
+    var nama = prompt('masukkan nama: ');
+    alert('halo ' + nama);
+
+    lagi = confirm('coba lagi?');
+}
+
+alert('Terima Kasih');
