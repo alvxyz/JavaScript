76c65a79442077a14c8a7825f5236ed39b415faa
Alvian Teddy Cahya Putra <alvianteddy@gmail.com>
2019-06-11 22:37:40 +0700

    Create angkot5.js

---
+++ Pengulangan/angkot5.js
@@ -0,0 +1,14 @@
+var jmlAngkot = 10;
+var angkotBeroperasi = 6;
+var noAngkot = 1;
+var angkotLembur = 8;
+
+for (var noAngkot; noAngkot <= jmlAngkot; noAngkot++) {
+    if (noAngkot <= angkotBeroperasi) {
+        console.log("Angkot No. " + noAngkot + " beroperasi dengan baik.")
+    } else if (noAngkot === angkotLembur || noAngkot === 10) {
+        console.log("Angkot No. " + noAngkot + " sedang lembur.")
+    } else {
+        console.log("Angkot No. " + noAngkot + " sedang tidak beroperasi.")
+    }
+}
