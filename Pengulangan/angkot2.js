1d24b0e3b1e69fc7b879ff986da09a5022dd9825
Alvian Teddy Cahya Putra <alvianteddy@gmail.com>
2019-06-11 09:25:49 +0700

    Membuat file angkot 2.js
    Membuat perulangan while dan for untuk menentukan angkot yang beroperasi dan yang tidak

---
+++ Pengulangan/angkot2.js
@@ -0,0 +1,12 @@
+var jmlAngkot = 10;
+var angkotBeroperasi = 6;
+var noAngkot = 1;
+
+while (noAngkot <= angkotBeroperasi){
+    console.log("Angkot No. " + noAngkot + " beroperasi dengan baik.")
+    noAngkot++;
+}
+
+for (var angkotBeroperasi = 7; angkotBeroperasi <= 10; angkotBeroperasi++){
+    console.log("Angkot No. " + angkotBeroperasi + " sedang tidak beroperasi.")
+}
